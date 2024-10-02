c9c9e8eaf57e8298832e9e66525e0cd08e4e2393
prayag123 <prayag1bisht@gmail.com>
2024-10-02 15:58:24 +0530

    version 1_2 oct

--- src/Oct/oct_2/Hello_word.java
+++ src/Oct/oct_2/Hello_word.java
@@ -1,10 +1,23 @@
 package Oct.oct_2;
-
+import java.util.Scanner;
 public class Hello_word {
 public static void main(String[] args)
 {
-    System.out.println("Hello word");
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    System.out.print("Enter the first number: ");
+    int num1 = scanner.nextInt();
+
+    System.out.print("Enter the second number: ");
+    int num2 = scanner.nextInt();
+
+
+    int sum = num1 + num2;
+
+
+    System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
 
 }
-
 }
